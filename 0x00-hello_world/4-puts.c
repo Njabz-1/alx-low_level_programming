be7a9ed1595dbfb0bba9d14d736c433cd353f09e
Njabz-1 <43475741+Njabz-1@users.noreply.github.com>
2023-02-16 21:37:05 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,15 @@
-#include <stdio.h>\n\nint main(void) {\nputs("\"Programming is like building a multilingual puzzle");\nreturn (0);\n}
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
