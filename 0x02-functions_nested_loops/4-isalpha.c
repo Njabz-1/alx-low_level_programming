78f3e130187b9905ab9f99eb193c454e43a62ae7
Njabz-1 <43475741+Njabz-1@users.noreply.github.com>
2023-02-21 19:09:47 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,8 +11,12 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
-	return (0);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
