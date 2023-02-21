ffe2436429c5e66f30a9eef1c3ac2796ce9b2481
Njabz-1 <43475741+Njabz-1@users.noreply.github.com>
2023-02-21 19:36:52 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#include"main.h"
-
-/**
- *
- * Return: lastDigit
-*/
-
-int print_last_digit(int n)
-{
-	int lastDigit;
-
-	if (n < 0)
-		lastDigit = -1 * (n % 10);
-	else
-		lastDigit = n % 10;
-
-	_putchar(lastDigit + '0');
-	return (lastDigit);
-}
-=======
 #include"main.h"
 
 /**
@@ -41,4 +20,3 @@ int print_last_digit(int n)
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
->>>>>>> e295acf14e4da23f60709f4f6f2d0c21569cd106
