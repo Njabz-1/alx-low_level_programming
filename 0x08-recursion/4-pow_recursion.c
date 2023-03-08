724883c38da29a19b75f5a4ae8533b32ef221987
Njabulo <mhlambinjabulo@gmail.com>
2023-03-08 19:27:01 +0200

    Open brace on next line

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,7 +17,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else if (y % 2 == 0)	{
+	else if (y % 2 == 0)
+	{
 		even = _pow_recursion(x, y / 2);
 		return (even * even);
 	}
