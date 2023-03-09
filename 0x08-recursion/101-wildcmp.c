319396ab130335f55c02614e55b6e93f1585b644
Njabulo <mhlambinjabulo@gmail.com>
2023-03-09 20:03:01 +0200

    Bonus

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,13 +9,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-    if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-        return (0);
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-    if (*s2 == '*')
-        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-    return (0);
+	if (*s1 == '\0' && *s2 == '\0') /* Base case */
+		return (1);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0') /* Optimization */
+		return (0);
+	if (*s1 == *s2) /* Match case */
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*') /* Wildcard case */
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0); /* Mismatch case */
 }
