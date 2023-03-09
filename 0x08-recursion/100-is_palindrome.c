75cb04c5675b37ac8df79f78279714389a100604
Njabulo <mhlambinjabulo@gmail.com>
2023-03-09 19:35:17 +0200

    Fixes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,9 +13,9 @@
 int check_palindrome(char *s, int start, int end)
 {
     if (start >= end)
-        return 1;
+        return (1);
     if (s[start] != s[end])
-        return 0;
+        return (0);
     return (check_palindrome(s, start + 1, end - 1));
 }
 
