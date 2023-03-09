a493eb1b05703bd63b84cefd297b4f34bb5a3a79
Njabulo <mhlambinjabulo@gmail.com>
2023-03-09 19:20:29 +0200

    Bonus

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * check_palindrome - The function checks if the characters at start and end match, and recursively calls itself with the updated indices if they do
+ * @s: string to evaluate
+ * @start: starting index
+ * @end: ending index
+ * Return: the resulting square root
+ */
+
+int check_palindrome(char *s, int start, int end) {
+    if (start >= end) {
+        return 1;
+    }
+    if (s[start] != s[end]) {
+        return 0;
+    }
+    return check_palindrome(s, start + 1, end - 1);
+}
+
+/**
+ * is_palindrome - checks if string is palindrome
+ * @s: string
+ *
+ * Return: 1 if s is a palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+    int len = strlen(s);
+    return check_palindrome(s, 0, len - 1);
+}
